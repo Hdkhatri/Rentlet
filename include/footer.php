fe0603cc190985646d8821a438dd3b0f3edf0d63
Hdkhatri <84757953+Hdkhatri@users.noreply.github.com>
2023-08-05 13:49:29 +0530

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,72 @@
+<footer class="full-row  p-0" style="background-color:#f0f0f0">
+            <div class="container">
+                <div  class="row">
+                    <div class="col-lg-12">
+                        <div class="divider py-40">
+                            <div class="row">
+                                <div class="col-md-12 col-lg-4">
+                                    <div class="footer-widget mb-4">
+                                        <div class="footer-logo mb-4"> <a href="#"><img class="logo-bottom" src="images/logo/greyLogo.png" alt="image" height=100px></a> </div>
+                                        <p class="pb-20 text-black">RENTLET is a tech based start-up, for all the people who are surrounded by lot of rental problems. We create a solution to hassles involved with renting a home. Simplification of rental process, delighting people, creating seamless and comfortable environment. We provide brokerage free rooms and apartments within premium properties. We provide SMOOTH renting experience. Our goal is SPACIOUS, SECURE, and AMENTIES filled housing options.</p>
+										
+                                        </div>
+                                </div>
+                                <div class="col-md-12 col-lg-8">
+                                    <div class="row">
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget footer-nav mb-4">
+                                                <h4 class="widget-title text-black double-down-line-left position-relative">Support</h4>
+                                                <ul class="hover-text-primary">
+                                                    <li><a href="#" class="text-black">Forum</a></li>
+                                                    <li><a href="#" class="text-black">Statistics</a></li>
+                                                    <li><a href="#" class="text-black">Terms and Condition</a></li>
+                                                    <li><a href="#" class="text-black">Get Support</a></li>
+                                                    <li><a href="#" class="text-black">Freequenly Ask Question</a></li>
+                                                    <li><a href="#" class="text-black">Contact</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget footer-nav mb-4">
+                                                <h4 class="widget-title text-black double-down-line-left position-relative">Quick Links</h4>
+                                                <ul class="hover-text-primary">
+                                                    <li><a href="#" class="text-black">About Us</a></li>
+                                                    <li><a href="#" class="text-black">Featured Property</a></li>
+                                                    <li><a href="#" class="text-black">Become A Member</a></li>
+                                                    <li><a href="#" class="text-black">Submit Property</a></li>
+                                                    <li><a href="#" class="text-black">How It Work</a></li>
+                                                    <li><a href="#" class="text-black">Our Agents</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget">
+                                                <h4 class="widget-title text-black double-down-line-left position-relative">Contact Us</h4>
+                                                <ul class="text-black">
+                                                    <li class="hover-text-primary"><i class="fas fa-map-marker-alt text-black mr-2 font-13 mt-1"></i>Pune,Ahmedabad</li>
+                                   
+													<li class="hover-text-primary"> <i class="fas fa-phone-alt text-black mr-2 font-13 mt-1"></i>+91 8087616404</li>
+                                                    <li class="hover-text-primary"><i class="fas fa-envelope text-black mr-2 font-13 mt-1"></i>support@rentlet.co.in</li>
+                                                  <li class="hover-text-primary"><i class="fas fa-envelope text-black mr-2 font-13 mt-1"></i>rentlet.service@gmail.com</li>
+                                                </ul>
+                                            </div>
+                                            <div class="footer-widget media-widget mt-4 text-black hover-text-primary"> <a href="https://www.facebook.com/profile.php?id=100092263473953"><i class="fab fa-facebook-f"></i></a> <a href="#"><i class="fab fa-twitter"></i></a> <a href="#"><i class="fab fa-google-plus-g"></i></a> <a href="#"><i class="fab fa-linkedin-in"></i></a> <a href="#"><i class="fas fa-rss"></i></a> </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row copyright">
+                    <div class="col-sm-6"> <span class="text-black">© 2023 PMK</span> </div>
+                    <div class="col-sm-6">
+                        <ul class="line-menu text-black hover-text-primary float-right">
+                            <li><a href="./privacy_policy.html">Privacy & Policy</a></li>
+                            
+                            
+                        </ul>
+                    </div>
+                </div>
+            </div>
+</footer>
